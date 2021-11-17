24735f4bf6cf73cb3b426348cb354cfe80640840
annora <annora@LAPTOP-624LMDFD.localdomain>
2021-11-17 20:14:40 +0800

    CRUD integration

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/articles' => 'articles#index'
 
+resources :articles
 end
+
+  # get '/articles' => 'articles#index'
+
+  # get '/articles/new' => 'articles#new'
+  # post '/articles' => 'articles#create'
+
+  # get '/articles/:id/edit' => 'articles#edit', as: edit_article
+
+  # get '/articles/:id' => 'articles#show'
+  # #get '/articles/two' => 'articles#two'
